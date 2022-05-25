da59d8fd6d28cb1915a915f3b9a01d0a80fad4c4
Ed0522 <ed0522@sina.cn>
2022-05-25 21:00:44 +0800

    1

--- websites/NewHKSVocabularyApp/js/toc.js
+++ websites/NewHKSVocabularyApp/js/toc.js
@@ -1,1 +1,1 @@
-document.write('<h6 style="margin-top: 1em;">Ŀ¼</h6><ul><li><a href="#1">1.  NewHSK3.0VocabularyApp</a></li></ul></ol>')
+document.write('<h6 style="margin-top: 1em;">Table of Contents</h6><ul><li><a href="#1">1.  New HSK 3.0 Vocabulary App</a></li></ul></ol>')
